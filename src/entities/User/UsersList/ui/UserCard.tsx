918bb60163cc5db6151a1f2f66940c4778eb9506
Jhon-Crow <jhamish@mail.ru>
2025-10-02 14:50:55 +0300

    refactored

---
+++ src/entities/User/UsersList/ui/UserCard.tsx
@@ -0,0 +1,45 @@
+import {Avatar, Card, CardActions, CardContent, Skeleton, Typography} from "@mui/material";
+import type {User} from "../../../../shared/types/user.ts";
+import type {ReactNode} from "react";
+
+export const UserCard = ({user, isLoading, actionButton}: {
+    user?: User,
+    isLoading: boolean,
+    actionButton?: ReactNode
+}) => {
+    if (isLoading) {
+        return (
+            <Card sx={{margin: '10px'}}>
+                <CardContent>
+                    <Typography variant="h6">
+                        <Skeleton animation="pulse" height={30} width="100%"/>
+                    </Typography>
+                    <Avatar src="" alt=""/>
+                    <Typography variant="body2">
+                        <Skeleton animation="pulse" height={20} width="100%"/>
+                    </Typography>
+                    <Typography variant="body2">
+                        <Skeleton animation="pulse" height={20} width="100%"/>
+                    </Typography>
+                </CardContent>
+            </Card>
+        );
+    }
+    if (!user) return null;
+    return (
+        <Card key={user.id} sx={{margin: '10px', display: 'flex'}}>
+            <CardContent>
+                <Typography variant="h6">{user.name}</Typography>
+                <Avatar src={user.avatar} alt={user.name}/>
+                <Typography variant="body2">Created at: {user.createdAt}</Typography>
+                <Typography variant="body2">ID: {user.id}</Typography>
+            </CardContent>
+            {actionButton ?
+                <CardActions>
+                    {actionButton}
+                </CardActions>
+                : null}
+
+        </Card>
+    );
+};
--- src/entities/User/UsersList/ui/UsersList.tsx
+++ src/entities/User/UsersList/ui/UsersList.tsx
@@ -1,46 +1,24 @@
 import {useGetPaginatedUsersQuery} from "../../../../app/redux/mockApi.ts";
 import {ErrorAlert} from "../../../../shared/ErrorAlert/ui/ErrorAlert.tsx";
-import {Avatar, Card, CardContent, Skeleton, Typography} from "@mui/material";
 import type {User} from "../../../../shared/types/user.ts";
+import {UserCard} from "./UserCard.tsx";
 
 interface Props {
     page: number;
 }
+
+
 export const UsersList = ({page = 1}: Props) => {
     const {data = [], isLoading, error} = useGetPaginatedUsersQuery(page);
+    if (isLoading) return (
+        [...Array(10)].map((_, i) => <UserCard key={i} isLoading={isLoading}/>)
+    )
     if (error) { // @ts-ignore
         return <ErrorAlert text={`${error.status} ${error.data}`}/>;
     }
-    if (isLoading) {
-        return (
-            <div>
-                {[...Array(10)].map((_, i) => <Card key={i} sx={{margin: '10px'}}>
-                    <CardContent>
-                        <Typography variant="h6">
-                            <Skeleton animation="pulse" height={30} width="100%"/>
-                        </Typography>
-                        <Avatar src="" alt=""/>
-                        <Typography variant="body2">
-                            <Skeleton animation="pulse" height={20} width="100%"/>
-                        </Typography>
-                        <Typography variant="body2">
-                            <Skeleton animation="pulse" height={20} width="100%"/>
-                        </Typography>
-                    </CardContent>
-                </Card>)}
-            </div>
-        );
-    }
     return (
         <div>
-            {data.length ? data.map((user: User) => <Card key={user.id} sx={{margin: '10px'}}>
-                <CardContent>
-                    <Typography variant="h6">{user.name}</Typography>
-                    <Avatar src={user.avatar} alt={user.name}/>
-                    <Typography variant="body2">Created at: {user.createdAt}</Typography>
-                    <Typography variant="body2">ID: {user.id}</Typography>
-                </CardContent>
-            </Card>) : null}
+            {data.length ? data.map((user: User) => <UserCard key={user.id} user={user} isLoading={isLoading}/>) : null}
         </div>
-    );
+    )
 };
